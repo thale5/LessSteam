38be8328d8ca7dd7376a342b527ea4437c719897
thale5 <thale5@users.noreply.github.com>
2017-08-17 17:38:51 +0300

    Inactive for now.

--- LessSteamTraffic/Mod.cs
+++ LessSteamTraffic/Mod.cs
@@ -2,29 +2,9 @@
 
 namespace LessSteam
 {
-    public sealed class Mod : LoadingExtensionBase, IUserMod
+    public sealed class Mod : IUserMod
     {
         public string Name => "Less Steam";
-        public string Description => "Faster startup";
-        public void OnEnabled() => LessTraffic.Setup();
-
-        public void OnDisabled()
-        {
-            if (LessTraffic.instance != null)
-                LessTraffic.instance.Dispose();
-        }
-
-        public void OnSettingsUI(UIHelperBase helper) => Settings.OnSettingsUI(helper);
-
-        public override void OnLevelLoaded(LoadMode mode)
-        {
-            Settings.helper = null;
-
-            if (LessTraffic.instance != null)
-            {
-                LessTraffic.instance.disableAds = Settings.settings.disableAdPanel;
-                LessTraffic.instance.pending.Clear();
-            }
-        }
+        public string Description => "Currently inactive";
     }
 }
